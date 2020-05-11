937ae4b0e97a2cdd18a8cde9e8356ce065933af8
Romain DARY - merlin <romain.dary@gmail.com>
2020-05-11 15:24:29 +0200

    chore: simplify code

--- Dockerfile
+++ Dockerfile
@@ -9,13 +9,13 @@ ENV BASE_URL="https://get.helm.sh"
 ENV TAR_FILE="helm-${VERSION}-linux-amd64.tar.gz"
 
 RUN apk add --update --no-cache git curl ca-certificates && \
+    echo ${BASE_URL}/${TAR_FILE} && \
     curl -L ${BASE_URL}/${TAR_FILE} | tar xvz && \
     mv linux-amd64/helm /usr/bin/helm && \
     chmod +x /usr/bin/helm && \
     rm -rf linux-amd64 && \
     rm -f /var/cache/apk/*
 
-RUN echo $VERSION
 RUN if [[ $(echo $VERSION | cut -d '.' -f 1) == "v2" ]]; then helm init --client-only; fi
 RUN helm plugin install https://github.com/chartmuseum/helm-push.git
 
--- build.sh
+++ build.sh
@@ -8,72 +8,58 @@
 
 set -e
 
-image="renaultdigital/helm"
-repo="helm/helm"
+IMAGE="renaultdigital/helm"
+REPOSITORY="helm/helm"
 
 build() {
-  echo "Found new version, building the image ${image}:${tag}"
-  docker build --no-cache --build-arg VERSION="${tag}" -t ${image}:"${tag}" .
+  tag=$1
 
-  # run test
-  version=$(docker run -ti --rm "${image}:${tag}" version --client)
+  echo "Found new version, building the image ${IMAGE}:${tag}"
+  docker build --no-cache --build-arg VERSION="${tag}" -t ${IMAGE}:"${tag}" .
 
-  if [[ "${version}" == *"Error: unknown flag: --client"* ]]; then
-    echo "Detected Helm >= 3"
-    version=$(docker run -ti --rm "${image}:${tag}" version | cut -d"\"" -f2 | cut -d"\"" -f1)
-    # version.BuildInfo{Version:"v3.0.0-beta.2", GitCommit:"26c7338408f8db593f93cd7c963ad56f67f662d4", GitTreeState:"clean", GoVersion:"go1.12.9"}
+  if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+    docker push ${IMAGE}:"${tag}"
+
   else
-    echo "Detected Helm < 3"
-    # Client: &version.Version{SemVer:"v2.9.0-rc2", GitCommit:"08db2d0181f4ce394513c32ba1aee7ffc6bc3326", GitTreeState:"clean"}
-    version=$(echo "${version}"| cut -d"\"" -f2 | cut -d"\"" -f1)
-  fi
-
-  if [ "${version}" == "${tag}" ]; then
-    echo "matched"
-
-    if [[ "$TRAVIS_BRANCH" == "master" ]]; then
-      docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-      docker push ${image}:"${tag}"
-    fi
-  else
-    echo "unmatched"
+    echo "Not on master, ignore push"
   fi
 }
 
 if [[ ${CI} == 'true' ]]; then
-  tags=$(curl -sL -H "Authorization: token ${API_TOKEN}"  https://api.github.com/repos/${repo}/releases |jq -r ".[].tag_name"| cut -c 1-)
+  tags=$(curl -sL -H "Authorization: token ${API_TOKEN}"  https://api.github.com/repos/${REPOSITORY}/releases |jq -r ".[].tag_name"| cut -c 1-)
 else
-  tags=$(curl -sL https://api.github.com/repos/${repo}/releases |jq -r ".[].tag_name"| cut -c 1-)
+  tags=$(curl -sL https://api.github.com/repos/${REPOSITORY}/releases |jq -r ".[].tag_name"| cut -c 1-)
 fi
 
 for tag in ${tags}
 do
   echo "$tag"
-  status=$(curl -sL https://hub.docker.com/v2/repositories/${image}/tags/"${tag}")
+  status=$(curl -sL https://hub.docker.com/v2/repositories/${IMAGE}/tags/"${tag}")
   echo "$status"
   if [[ "${status}" =~ "not found" ]]; then
-    build
+    build "$tag"
   fi
 done
 
 echo "Get latest version based on the latest Github release"
 
 if [[ ${CI} == 'true' ]]; then
-  latest=$(curl -sL -H "Authorization: token ${API_TOKEN}"  https://api.github.com/repos/${repo}/releases/latest |jq -r ".tag_name"| cut -c 2-)
+  latest=$(curl -sL -H "Authorization: token ${API_TOKEN}"  https://api.github.com/repos/${REPOSITORY}/releases/latest |jq -r ".tag_name"| cut -c 2-)
 else
-  latest=$(curl -sL https://api.github.com/repos/${repo}/releases/latest |jq -r ".tag_name"| cut -c 2-)
+  latest=$(curl -sL https://api.github.com/repos/${REPOSITORY}/releases/latest |jq -r ".tag_name"| cut -c 2-)
 fi
 
-digest=$(curl -sL https://hub.docker.com/v2/repositories/${image}/tags/"${latest}" | jq -r ".images[].digest" )
-digest_latest=$(curl -sL https://hub.docker.com/v2/repositories/${image}/tags/latest | jq -r ".images[].digest" )
+digest=$(curl -sL https://hub.docker.com/v2/repositories/${IMAGE}/tags/"${latest}" | jq -r ".images[].digest" )
+digest_latest=$(curl -sL https://hub.docker.com/v2/repositories/${IMAGE}/tags/latest | jq -r ".images[].digest" )
 
 if [ "${digest_latest}" != "${digest}" ]; then
   echo "Update latest image to ${latest}"
   if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == false ]]; then
     docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-    docker pull ${image}:"${latest}"
-    docker tag ${image}:"${latest}" ${image}:latest
-    docker push ${image}:latest
+    docker pull ${IMAGE}:"${latest}"
+    docker tag ${IMAGE}:"${latest}" ${IMAGE}:latest
+    docker push ${IMAGE}:latest
   fi
 else
   echo "Nothing to do !"
